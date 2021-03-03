ac1002b9d75280013d40434320cd936b9a26b24f
Ivan Tanev <vankata.t@gmail.com>
2021-03-03 23:16:27 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'G-P6MN4QVLD4'
       },
     },
     `gatsby-plugin-feed`,
